0be9a0b95a5e51967d2361c427a75d2172e75ff7
Chrissy Harmon <clio.christine.harmon@gmail.com>
2020-06-10 12:10:06 -0400

    we've done the first few exercises

---
+++ src/api.js
@@ -0,0 +1,11 @@
+const BASE_URL = 'https://thinkful-list-api.herokuapp.com/chatchawan-chrissy';
+
+const getItems = function () {
+  return Promise.resolve('A successful response!');
+};
+
+
+
+export default {
+    getItems
+};
